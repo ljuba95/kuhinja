39d202d13843160be72e479540114d62dcd71cc2
Milos Ljubisavljevic <ljuba95@hotmail.com>
2017-12-20 21:24:05 +0100

    public

--- view/global/css.php
+++ view/global/css.php
@@ -1,7 +1,7 @@
-<link href="/public/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<link href="/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"
       integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 
 <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.min.css" rel="stylesheet" />
 
-<link rel="stylesheet" href="/public/css/default.css" type="text/css"/>
+<link rel="stylesheet" href="/css/default.css" type="text/css"/>
